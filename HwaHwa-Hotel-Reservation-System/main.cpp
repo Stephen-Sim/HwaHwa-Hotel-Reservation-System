a9671388761d280b530e18cb79a21e51e92da766
Stephen-Sim <simshansiong2002@gmail.com>
2024-12-03 23:09:00 +0800

    update entry point

--- HwaHwa-Hotel-Reservation-System/main.cpp
+++ HwaHwa-Hotel-Reservation-System/main.cpp
@@ -1,14 +1,10 @@
 #include "Menu.h"
-#include "Customer.h"
-#include "Admin.h"
 
 int main()
 {
     system("color 57");
 
-    // Menu();
-    // Admin();
-    Admin::adminLogin();
+    Menu();
 
     return 0;
 }
